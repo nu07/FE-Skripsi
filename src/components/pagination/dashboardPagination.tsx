7ec0426564ce2777403b16ecb317befed9231e8f
nu_vandalstik <wisnukristanto321@gmail.com>
2025-06-10 22:34:37 +0700

    fix button on mobile functiobn

--- src/components/pagination/dashboardPagination.tsx
+++ src/components/pagination/dashboardPagination.tsx
@@ -3,18 +3,30 @@ function dashboardPagination({ pagination, setPagination }: any) {
     <div>
       <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
         <div className="flex-1 flex justify-between sm:hidden">
-          <a
-            href="#"
+          <button
+            disabled={pagination.currentPages === 1}
+            onClick={() => {
+              setPagination((prev: any) => ({
+                ...prev,
+                currentPages: prev.currentPages - 1,
+              }));
+            }}
             className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
           >
             Previous
-          </a>
-          <a
-            href="#"
+          </button>
+          <button
+            disabled={pagination.currentPages === pagination.totalPages}
+            onClick={() => {
+              setPagination((prev: any) => ({
+                ...prev,
+                currentPages: prev.currentPages + 1,
+              }));
+            }}
             className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
           >
             Next
-          </a>
+          </button>
         </div>
         <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
           <div>
@@ -31,8 +43,8 @@ function dashboardPagination({ pagination, setPagination }: any) {
               aria-label="Pagination"
             >
               <button
-                disabled={pagination.currentPages === 1}
                 className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                disabled={pagination.currentPages === 1}
                 onClick={() => {
                   setPagination((prev: any) => ({
                     ...prev,
@@ -68,8 +80,8 @@ function dashboardPagination({ pagination, setPagination }: any) {
               ))}
 
               <button
-                disabled={pagination.currentPages === pagination.totalPages}
                 className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                disabled={pagination.currentPages === pagination.totalPages}
                 onClick={() => {
                   setPagination((prev: any) => ({
                     ...prev,
--- src/pages/admin/news.tsx
+++ src/pages/admin/news.tsx
@@ -53,94 +53,98 @@ function NewsAdmin() {
     getAllNews();
   }, [pagination.currentPages]);
 
-  return (
-    <>
-      <ModalDetail
-        isOpen={isOpenDetail}
-        setIsOpen={setIsOpenDetail}
-        data={isDetailData}
-      />
-      <ul
-        role="list"
-        className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
-      >
-        {allDataNews?.map((person) => (
-          <li
-            key={person.id}
-            className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
-          >
-            <div className="w-full flex items-center justify-between p-6 space-x-6">
-              <div className="flex-1 truncate">
-                <div className="flex items-center space-x-3">
-                  <h3 className="text-gray-900 text-sm font-medium truncate">
-                    {person.title}
-                  </h3>
-                  <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
-                    {person.admin.nama}
-                  </span>
+  if (pagination.isLoading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <>
+        <ModalDetail
+          isOpen={isOpenDetail}
+          setIsOpen={setIsOpenDetail}
+          data={isDetailData}
+        />
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+        >
+          {allDataNews?.map((person) => (
+            <li
+              key={person.id}
+              className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
+            >
+              <div className="w-full flex items-center justify-between p-6 space-x-6">
+                <div className="flex-1 truncate">
+                  <div className="flex items-center space-x-3">
+                    <h3 className="text-gray-900 text-sm font-medium truncate">
+                      {person.title}
+                    </h3>
+                    <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                      {person.admin.nama}
+                    </span>
+                  </div>
+                  <p className="mt-1 text-gray-500 text-sm truncate">
+                    {person.content}
+                  </p>
                 </div>
-                <p className="mt-1 text-gray-500 text-sm truncate">
-                  {person.content}
-                </p>
+                <img
+                  className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
+                  src={person.admin.nama}
+                  alt=""
+                />
               </div>
-              <img
-                className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
-                src={person.admin.nama}
-                alt=""
-              />
-            </div>
-            <div>
-              <div className="-mt-px flex divide-x divide-gray-200">
-                <div className="w-0 flex-1 flex">
-                  <Button
-                    onClick={() => {
-                      setisDetailData(person);
-                      setIsOpenDetail(true);
-                    }}
-                    className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
-                  >
-                    <DocumentIcon
-                      className="w-5 h-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <span className="ml-3">Detail</span>
-                  </Button>
-                </div>
-                <div className="w-0 flex-1 flex">
-                  <Link
-                    to={`mailto:${person.admin.nama}`}
-                    className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
-                  >
-                    <DocumentTextIcon
-                      className="w-5 h-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <span className="ml-3">Edit</span>
-                  </Link>
-                </div>
-                <div className="-ml-px w-0 flex-1 flex">
-                  <Link
-                    to={`tel:${person.admin.nama}`}
-                    className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
-                  >
-                    <DocumentRemoveIcon
-                      className="w-5 h-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <span className="ml-3">Delete</span>
-                  </Link>
+              <div>
+                <div className="-mt-px flex divide-x divide-gray-200">
+                  <div className="w-0 flex-1 flex">
+                    <Button
+                      onClick={() => {
+                        setisDetailData(person);
+                        setIsOpenDetail(true);
+                      }}
+                      className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+                    >
+                      <DocumentIcon
+                        className="w-5 h-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-3">Detail</span>
+                    </Button>
+                  </div>
+                  <div className="w-0 flex-1 flex">
+                    <Link
+                      to={`mailto:${person.admin.nama}`}
+                      className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+                    >
+                      <DocumentTextIcon
+                        className="w-5 h-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-3">Edit</span>
+                    </Link>
+                  </div>
+                  <div className="-ml-px w-0 flex-1 flex">
+                    <Link
+                      to={`tel:${person.admin.nama}`}
+                      className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+                    >
+                      <DocumentRemoveIcon
+                        className="w-5 h-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-3">Delete</span>
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <DashboardPagination
-        pagination={pagination}
-        setPagination={setPagination}
-      />
-    </>
-  );
+            </li>
+          ))}
+        </ul>
+        <DashboardPagination
+          pagination={pagination}
+          setPagination={setPagination}
+        />
+      </>
+    );
+  }
 }
 
 export default NewsAdmin;
